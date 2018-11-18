9cb3dd99c9ba2d3ed19dfa53ca17fb7f57d3809f
faraday77 <resha.mau11@gmail.com>
2018-11-17 21:06:40 -0700

    First Python program to commit to this repository. Hooray!

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+# NOVEMBER 17, 2018 (SATURDAY)
+# TIME: 9:03 PM MST
+# ------------------------------------
+#
+# This is an example Python 3 program. Testing to see if I can manage a Git-based habit of coding.
+#
+# ------------------------------------
+
+print("Hello World!")
+
+
